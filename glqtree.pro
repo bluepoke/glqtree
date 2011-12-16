8868f17d2569c119426f55faabb1a3547a78aeb2
Peter Kossek <kossek@gmx.de>
2011-12-16 10:02:01 +0100

    first commit

---
+++ glqtree.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-12-15T14:37:12
+#
+#-------------------------------------------------
+
+QT       += core gui opengl
+
+TARGET = glqtree
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    mypanelopengl.cpp
+
+HEADERS  += mainwindow.h \
+    mypanelopengl.h
+
+FORMS    += mainwindow.ui
+
+
---
+++ mypanelopengl.h
@@ -0,0 +1,30 @@
+#ifndef MYPANELOPENGL_H
+#define MYPANELOPENGL_H
+
+#include <QGLWidget>
+
+class MyPanelOpenGL : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit MyPanelOpenGL(QWidget *parent = 0);
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+
+private:
+    int sides;
+    double radius;
+
+
+signals:
+
+public slots:
+    void changeSides(int s);
+    void changeRadius(double r);
+
+};
+
+#endif // MYPANELOPENGL_H
